a92daf4a4bebe4399230ec2c4f6e6d419899891d
Ramya Canessane <ramyacanessane@icloud.com>
2026-07-29 14:54:14 +0200

    Initial Commit

---
+++ CultureTrek/CultureTrekApp.swift
@@ -0,0 +1,17 @@
+//
+//  CultureTrekApp.swift
+//  CultureTrek
+//
+//  Created by Canessane Ramya on 29/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct CultureTrekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
