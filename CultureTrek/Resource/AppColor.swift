313d82e64673f31a5e541d17b4d19382fd590d77
Mathieu Nivelles <mathieu.nivelles@gmail.com>
2026-08-01 23:53:22 +0200

    style: add design tokens. Add colors in Assets catalog. Create two utils object : AppColor and AppToken.

---
+++ CultureTrek/Resource/AppColor.swift
@@ -0,0 +1,42 @@
+//
+//  AppColor.swift
+//  CultureTrek
+//  
+//  Created by Mathieu Nivelles on 01/08/2026.
+//  Copyright © 2026 Mathieu Nivelles. All rights reserved.
+//
+
+import SwiftUI
+
+enum AppColor {
+    
+    static let accentNeutral: Color = .primitiveAccentNeutral
+    static let accentPrimary: Color = .primitiveAccentPrimary
+    static let accentSecondary: Color = .primitiveAccentSecondary
+    static let background: Color = .primitiveBackground
+    static let border: Color = .primitiveBorder
+    static let disabledBackground: Color = .primitiveDisabledBackground
+    static let disabledLabel: Color = .primitiveDisabledLabel
+    static let error: Color = .primitiveError
+    static let info: Color = .primitiveInfo
+    static let inverseBackground: Color = .primitiveInverseBackground
+    static let inverseForegroundPrimary: Color = .primitiveInverseForegroundPrimary
+    static let inverseForegroundSecondary: Color = .primitiveInverseForegroundSecondary
+    static let liked: Color = .primitiveLiked
+    static let popupBadAnswerBackground: Color = .primitivePopupBadAnswerBackground
+    static let popupBadAnswerForeground: Color = .primitivePopupBadAnswerForeground
+    static let popupGoodAnswerBackground: Color = .primitivePopupGoodAnswerBackground
+    static let popupGoodAnswerForeground: Color = .primitivePopupGoodAnswerForeground
+    static let shadow: Color = .primitiveShadow
+    static let success: Color = .primitiveSuccess
+    static let warning: Color = .primitiveWarning
+    
+    enum Page {
+        static let background: Color = AppColor.background
+    }
+    
+    enum Label {
+        static let primary: Color = .labelPrimary
+        static let secondary: Color = .labelSecondary
+    }
+}
---
+++ CultureTrek/Resource/AppToken.swift
@@ -0,0 +1,59 @@
+//
+//  AppToken.swift
+//  CultureTrek
+//  
+//  Created by Mathieu Nivelles on 01/08/2026.
+//  Copyright © 2026 Mathieu Nivelles. All rights reserved.
+//  
+
+enum AppToken {
+
+    static let shadowSize: Double = 4
+    static let shadowSizeMini: Double = 3
+    static let cornerRadius: Double = Primitive.radiusSM
+    static let borderWidth: Double = 2
+    static let popupBorderWidth: Double = 4
+
+    enum Primitive {
+        // Padding
+        static let padding0: Double = 0
+        static let padding1: Double = 4
+        static let padding2: Double = 8
+        static let padding3: Double = 12
+        static let padding4: Double = 16
+        static let padding5: Double = 20
+        static let padding6: Double = 24
+        static let padding8: Double = 32
+        static let padding10: Double = 40
+        static let padding12: Double = 48
+        static let padding16: Double = 64
+        static let padding20: Double = 80
+        
+        // Radius
+        static let radiusNone: Double = 0
+        static let radiusXS: Double = 2
+        static let radiusSM: Double = 4
+        static let radiusMD: Double = 6
+        static let radiusLG: Double = 8
+        static let radiusXL: Double = 12
+        static let radius2XL: Double = 16
+        static let radius3XL: Double = 20
+        static let radius4XL: Double = 24
+        static let radius5XL: Double = 32
+        static let radiusFull: Double = 9999
+        
+        // Spacing
+        static let spacing0: Double = 0
+        static let spacing1: Double = 4
+        static let spacing2: Double = 8
+        static let spacing3: Double = 12
+        static let spacing4: Double = 16
+        static let spacing5: Double = 20
+        static let spacing6: Double = 24
+        static let spacing8: Double = 32
+        static let spacing10: Double = 40
+        static let spacing12: Double = 48
+        static let spacing16: Double = 64
+        static let spacing20: Double = 80
+    }
+}
