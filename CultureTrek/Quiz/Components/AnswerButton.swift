1efcca24e6fd801858feb8440e515b42fc408985
Ramya Canessane <ramyacanessane@icloud.com>
2026-08-06 21:08:18 +0200

    feat(Quiz): add AnswerButton

---
+++ CultureTrek/Quiz/Components/AnswerButton.swift
@@ -0,0 +1,42 @@
+//
+//  AnswerButton.swift
+//  CultureTrek
+//
+//  Created by Canessane Ramya on 06/08/2026.
+//
+
+import SwiftUI
+
+struct AnswerButton: View {
+    @State private var isSelected: Bool = false
+    let label: String
+    
+    var body: some View {
+        Text(label)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .font(.spaceGrotesk(size: 17, weight: .medium))
+            .padding(16)
+            .border(AppColor.border, width: AppToken.borderWidth)
+            .background(
+                RoundedRectangle(cornerRadius: AppToken.cornerRadius)
+                    .fill(isSelected ? .primitiveAccentSecondary : .primitiveAccentNeutral)
+                    .shadow(color: isSelected ? .clear : AppColor.shadow,
+                            radius: 0,
+                            x: isSelected ? 0 : AppToken.shadowSize,
+                            y: isSelected ? 0 : AppToken.shadowSize
+                           )
+            )
+            .onTapGesture {
+                isSelected.toggle()
+            }
+            .animation(AppToken.buttonPressedAnimation, value: isSelected)
+            .offset(x: isSelected ? AppToken.shadowSize : 0,
+                    y: isSelected ? AppToken.shadowSize : 0
+            )
+    }
+}
+
+#Preview {
+    AnswerButton(label: "O tempora, o mores! Senatus haec intellegit, consul videt; hic tamen vivit.")
+        .padding(16)
+}
