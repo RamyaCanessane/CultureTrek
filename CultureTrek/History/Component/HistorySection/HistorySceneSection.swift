1cfc5acf6d0f06a9498de58f88d1c430a81fcb64
ĖVA VRANOVIČ <vooowitch@gmail.com>
2026-08-10 21:50:02 +0200

    feat(History) almost completed HistoryScene

---
+++ CultureTrek/History/Component/HistorySection/HistorySceneSection.swift
@@ -0,0 +1,31 @@
+//
+//  HistorySceneSection.swift
+//  CultureTrek
+//
+//  Created by Apprenant162 on 09/08/2026.
+//
+
+import SwiftUI
+
+struct HistorySceneSection: View {
+    let sectionTitle: Text
+    let city: String
+    let department: String?
+    let name: String
+    let picture: Image
+    let region: String
+    let isBig: Bool = true
+    
+    var body: some View {
+        VStack{
+            HistorySectionTitle(title: sectionTitle)
+            
+            TrekCell(city: city, department: department, name: name, picture: picture, region: region, isBig: true)
+            TrekCell(city: city, department: department, name: name, picture: picture, region: region, isBig: true)
+        }
+    }
+}
+
+#Preview {
+    HistorySceneSection(sectionTitle: Text("43 GSgf 3453"), city: "GSRG", department: "GS4Fge", name: "GZFfdaer", picture: Image(.trekTestPicture), region: "GDFZzc")
+}
