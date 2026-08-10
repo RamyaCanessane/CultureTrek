ce6d260d1e1e9846f9c581f50a147cf57c09bf40
ĖVA VRANOVIČ <vooowitch@gmail.com>
2026-08-10 21:52:11 +0200

    feat(History) correction of components
    Signed-off-by: ĖVA VRANOVIČ <vooowitch@gmail.com>

---
+++ CultureTrek/History/Component/HistoryHeader/FilterButton.swift
@@ -0,0 +1,38 @@
+//
+//  FilterButton.swift
+//  CultureTrek
+//
+//  Created by Apprenant162 on 07/08/2026.
+//
+
+import SwiftUI
+
+struct FilterButton: View {
+    let filterAction: () -> Void
+    
+    var body: some View {
+        VStack(alignment: .trailing){
+            Button(action: filterAction){
+                Image(systemName: FilterButtonStyles.filterIcon)
+                    .font(FilterButtonStyles.iconFont)
+                    .padding(FilterButtonStyles.padding)
+            }
+            .buttonStyle(.neubrutIcon(kind: .neutral))
+        }
+    }
+}
+
+struct FilterButtonStyles {
+    static let iconFont: Font = .system(size: 20,
+                                        weight: .heavy)
+    static let padding = AppToken.Primitive.padding1
+    static let filterIcon: String = "line.3.horizontal.decrease"
+}
+
+#Preview {
+    VStack {
+        FilterButton(filterAction: {})
+    }
+    .padding()
+    .background(AppColor.background)
+}
