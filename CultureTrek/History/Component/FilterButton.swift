7618ad754f95239d1844914aac76fcc4a141dc45
ĖVA VRANOVIČ <vooowitch@gmail.com>
2026-08-10 10:52:18 +0200

    feat(History) modification of local Components

--- CultureTrek/History/Component/FilterButton.swift
+++ CultureTrek/History/Component/FilterButton.swift
@@ -12,19 +12,35 @@ struct FilterButton: View {
     
     var body: some View {
         icon: do {
-            Image(systemName: filterIcon)
-                .font(FilterButtonStyles.iconFont)
-                .foregroundStyle(FilterButtonStyles.iconColor)
-                .padding(FilterButtonStyles.padding)
-                .frame(width: FilterButtonStyles.buttonSize,
-                       height: FilterButtonStyles.buttonSize)
-                .background(FilterButtonStyles.background)
-                .clipShape(FilterButtonStyles.shape)
-                .overlay {
-                    FilterButtonStyles.shape
-                        .strokeBorder(FilterButtonStyles.borderColor,
-                                      lineWidth: FilterButtonStyles.borderWidth)
-                }
+            Image(
+                systemName: filterIcon
+            )
+            .font(
+                FilterButtonStyles.iconFont
+            )
+            .foregroundStyle(
+                FilterButtonStyles.iconColor
+            )
+            .padding(
+                FilterButtonStyles.padding
+            )
+            .frame(
+                width: FilterButtonStyles.buttonSize,
+                height: FilterButtonStyles.buttonSize
+            )
+            .background(
+                FilterButtonStyles.background
+            )
+            .clipShape(
+                FilterButtonStyles.shape
+            )
+            .overlay {
+                FilterButtonStyles.shape
+                    .strokeBorder(
+                        FilterButtonStyles.borderColor,
+                        lineWidth: FilterButtonStyles.borderWidth
+                    )
+            }
         }
     }
 }
@@ -37,15 +53,19 @@ struct FilterButtonStyles {
     
     static let cornerRadius = AppToken.cornerRadius
     
-    static let iconFont: Font = .system(size: buttonSize / 2,
-                                        weight: .regular,
-                                        design: .default)
+    static let iconFont: Font = .system(
+        size: buttonSize / 2,
+        weight: .regular,
+        design: .default
+    )
     static let padding = AppToken.Primitive.padding1
     
     static let shadowColor = AppColor.shadow
     static let shadowSize = AppToken.shadowSize
     
-    static let shape = RoundedRectangle(cornerRadius: cornerRadius)
+    static let shape = RoundedRectangle(
+        cornerRadius: cornerRadius
+    )
     static let buttonSize: Double = AppToken.buttonHeight
     
     static let iconColor = AppColor.Label.primary
@@ -53,8 +73,12 @@ struct FilterButtonStyles {
 
 #Preview {
     VStack {
-        FilterButton(filterIcon: "line.3.horizontal.decrease")
+        FilterButton(
+            filterIcon: "line.3.horizontal.decrease"
+        )
     }
     .padding()
-    .background(AppColor.background)
+    .background(
+        AppColor.background
+    )
 }
