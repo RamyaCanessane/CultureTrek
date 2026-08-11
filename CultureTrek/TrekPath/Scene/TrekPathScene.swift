b07931f95e44388e192c9edf8e9435f144c1c7bf
Mathieu Nivelles <mathieu.nivelles@gmail.com>
2026-08-11 12:27:27 +0200

    feat(TrekPath): create TrekPathScene with PathCalculator

---
+++ CultureTrek/TrekPath/Scene/TrekPathScene.swift
@@ -0,0 +1,74 @@
+//
+//  TrekPathScene.swift
+//  CultureTrek
+//  
+//  Created by Mathieu Nivelles on 11/08/2026.
+//  Copyright © 2026 Mathieu Nivelles. All rights reserved.
+//  
+
+import MapKit
+import SwiftUI
+
+struct TrekPathScene: View {
+    let riddles: [Riddle]
+    let hasLastRiddle: Bool
+    
+    @State private var path: [MKRoute] = []
+    
+    var body: some View {
+        Map {
+            ForEach(riddles) { riddle in
+                Annotation("", coordinate: riddle.coordinate) {
+                    TrekPathMarker(riddle: riddle,
+                                   kind: getTrekPathMarkerKind(riddle: riddle))
+                }
+            }
+            
+            ForEach(path, id: \.self) { route in
+                MapPolyline(route)
+                    .stroke(Styles.pathColor,
+                            style: hasLastRiddle
+                            ? Styles.pathCompletedStrokeStyle
+                            : Styles.pathNotCompletedStrokeStyle)
+            }
+        }
+        .task {
+            self.path = await PathCalculator.getFullPath(with: coordinates)
+        }
+    }
+    
+    private var coordinates: [CLLocationCoordinate2D] {
+        riddles.map(\.coordinate)
+    }
+    
+    private func getTrekPathMarkerKind(riddle: Riddle) -> TrekPathMarker.Kind {
+        if isFirstRiddle(riddle) {
+            .first
+        } else if hasLastRiddle && isLastRiddle(riddle) {
+            .last
+        } else {
+            .other
+        }
+    }
+    
+    private func isFirstRiddle(_ riddle: Riddle) -> Bool {
+        riddles.first?.id == riddle.id
+    }
+    
+    private func isLastRiddle(_ riddle: Riddle) -> Bool {
+        riddles.last?.id == riddle.id
+    }
+}
+
+fileprivate enum Styles {
+    
+    static let pathColor: Color = .black
+    static let pathCompletedStrokeStyle = StrokeStyle(lineWidth: 5)
+    static let pathNotCompletedStrokeStyle = StrokeStyle(lineWidth: 2.5,
+                                                         dash: [2.5, 2.5])
+}
+
+#Preview {
+    TrekPathScene(riddles: Riddle.examples,
+                  hasLastRiddle: false)
+}
