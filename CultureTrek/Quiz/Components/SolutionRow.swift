652d7cbe5adf9a879ab2351d03dcaa1821b12568
Ramya Canessane <ramyacanessane@icloud.com>
2026-08-12 09:41:07 +0200

    feat(Quiz): create SolutionRow component

---
+++ CultureTrek/Quiz/Components/SolutionRow.swift
@@ -0,0 +1,62 @@
+//
+//  SolutionRow.swift
+//  CultureTrek
+//
+//  Created by Canessane Ramya on 11/08/2026.
+//
+
+import SwiftUI
+
+struct SolutionRow: View {
+    //    @State private var isTapped: Bool = false
+    @State private var isExpanded: Bool = false
+    //    let question: QuizQuestion = QuizQuestion.examples
+    let question: String
+    let answer: String
+    let explanation: String
+    
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: AppToken.Primitive.padding4) {
+                Text("1")
+                    .font(.spaceGrotesk(size: 20, weight: .medium))
+                    .frame(width: 40, height: 40)
+                    .background(AppColor.accentSecondary)
+                    .overlay(
+                        RoundedRectangle(cornerRadius: AppToken.cornerRadius)
+                            .stroke(AppColor.border, lineWidth: AppToken.borderWidth)
+                    )
+                VStack(alignment: .leading, spacing: 16) {
+                    Text(question)
+                        .font(.spaceGrotesk(size: 15, weight: .medium))
+                    Text(answer)
+                        .font(.spaceGrotesk(size: 15, weight: .bold))
+                }
+                
+                Button {
+                    isExpanded.toggle()
+                } label: {
+                    Image(systemName: "chevron.right")
+                        .rotationEffect(.degrees(isExpanded ? 90 : 0))
+                }
+                .tint(AppColor.Label.primary)
+            }
+            if isExpanded {
+                ExplanationAnswerCard(explanation: explanation)
+            }
+            
+        }
+        .padding(AppToken.Primitive.padding4)
+        .background(AppColor.accentNeutral)
+        .overlay(
+            RoundedRectangle(cornerRadius: AppToken.cornerRadius)
+                .stroke(AppColor.border, lineWidth: AppToken.borderWidth)
+            
+        )
+    }
+}
+
+#Preview {
+    SolutionRow(question: "En quelle année la Révolution française a-t-elle commencé avec la prise de la Bastille ?", answer: "1789", explanation: "La Fête nationale française commémore le 14 juillet 1789 (prise de la Bastille).")
+        .padding(16)
+}
