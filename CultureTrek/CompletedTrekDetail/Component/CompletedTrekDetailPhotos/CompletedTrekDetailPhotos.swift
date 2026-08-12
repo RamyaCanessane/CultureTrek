b22fda7b1d3d7b2fe7de6bd09df53107143ee91d
kays-dev <krys.b@outlook.com>
2026-08-12 10:51:45 +0200

    feature(CompletedTrekDetail): add CompletedTrekDetailPhotos. feature(CompletedTrekDetail): add CompletedTrekDetailCompletionData. feature(CompletedTrekDetail): add CompletedTrekDetailDate. feature(CompletedTrekDetail): add CompletedTrekDetailFooter.

---
+++ CultureTrek/CompletedTrekDetail/Component/CompletedTrekDetailPhotos/CompletedTrekDetailPhotos.swift
@@ -0,0 +1,18 @@
+//
+//  CompletedTrekDetailPhotos.swift
+//  CultureTrek
+//
+//  Created by Apprenant174 on 12/08/2026.
+//
+
+import SwiftUI
+
+struct CompletedTrekDetailPhotos: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CompletedTrekDetailPhotos()
+}
